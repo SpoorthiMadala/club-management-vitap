5f8f9c35dcd3498a7cfcaa5b505e2a6d467bc825
MADALA LEELA VENKATA SIVA SPOORTHI <mmadalavenkatesh@gmail.com>
2026-01-21 23:03:51 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://club-management-vitap.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
@@ -35,3 +35,4 @@ api.interceptors.response.use(
 );
 
 export default api;
+
